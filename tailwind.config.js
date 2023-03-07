fad1c5f7c59f4b804189c3da265462b97ca02bab
Andis <muchamad.andis.2105356@students.um.ac.id>
2023-03-07 17:51:08 +0700

    tambah view register

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    colors: {
+      'colorregister':'#03001C',
+      'square' : '#301E67',
+      'white' : '#fff',
+      'link' : '#5B8FB9',
+      'black' :'#000'
+    },
+    // extend: {},
+  },
+  plugins: [],
+}

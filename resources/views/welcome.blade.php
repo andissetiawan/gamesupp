3177a344d5df4f7374aeeece3530adade504ff89
Andis <muchamad.andis.2105356@students.um.ac.id>
2023-03-24 10:42:29 +0700

    landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,11 +11,114 @@
 
 </head>
 <body class="bg-colorregister ">
-<div class="bg-square w-full h-24 ">
-  <div>
-    
-  </div>
+<nav class="bg-square py-2 py-7">
+  <div class="flex max-w-9xl mx-auto px-4 sm:px-6 lg:px-8 items-center">
+  <a href="/">
+          <img class="px-5 w-full h-8" src="{{ asset ('img/logogs.png') }}" alt="Logo Brand">
+        </a>
+<div class="ml-10 mr-10">
+<div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+<div class="relative">
+  <input class="custom-input block rounded-full py-3 px-4 " id="search" type="text" placeholder="Search...">
+  
 </div>
+
+
+
+</div>
+</div>
+<div class="flex">
+<a href="{{ url('/masuk') }}" class=" bg-white text-square font-bold py-2 px-4 rounded-full border">
+  Masuk
+</a>
+<a href="{{ url('/daftar') }}" class=" bg-white ml-5 text-square font-bold py-2 px-4 rounded-full border">
+  Daftar
+</a>
+
+
+</div>
+</div>
+</nav>
+
+<!-- body -->
+<div class="flex justify-center">
+<div class=" w-10/12 mt-10 h-96 rounded-md bg-white">
+
+</div>
+</div>
+<div class="flex justify-center">
+<div class=" w-10/12 mt-10 h-96">
+<div style="display: flex; justify-content: space-between;">
+  <div>
+    <h3 class="text-5xl font-bold text-white">Terbaru</h3>
+  </div>
+  <div style="align-self: flex-end;">
+    <a class="text-white text-xl self-center flex" href="{{ url('/#') }}">View All &nbsp;
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+</svg>
+
+    </a>
+  </div>
+</div><div>View All</div>
+<!-- component -->
+<div class="flex flex-col m-auto p-auto mt-4">
+      <div
+        class="flex overflow-x-scroll pb-10 hide-scroll-bar"
+      >
+        <div
+          class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 "
+        >
+          <div class="inline-block px-3">
+            <div
+              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            ></div>
+          </div>
+          <div class="inline-block px-3">
+            <div
+              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            ></div>
+          </div>
+          <div class="inline-block px-3">
+            <div
+              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            ></div>
+          </div>
+          <div class="inline-block px-3">
+            <div
+              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            ></div>
+          </div>
+          <div class="inline-block px-3">
+            <div
+              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            ></div>
+          </div>
+          <div class="inline-block px-3">
+            <div
+              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            ></div>
+          </div>
+          <div class="inline-block px-3">
+            <div
+              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            ></div>
+          </div>
+          <div class="inline-block px-3">
+            <div
+              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            ></div>
+          </div>
+        </div>
+      </div>
+</div>
+
+
+
+</div>
+</div>
+
+
 
 </body>
 </html>
